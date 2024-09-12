1c4e7251b5099316613ac5f9a092609b20bc476e
Dwiki <dwikidrf@gmail.com>
2024-09-12 10:55:38 +0700

    second

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+//import reactLogo from './assets/react.svg'
+//import viteLogo from '/vite.svg'
+import myPicture from './assets/cekiki_alimufki.jpg'  // Add this line
 import './App.css'
 
 function App() {
@@ -8,23 +9,25 @@ function App() {
 
   return (
     <>
-      <div>
+      {/* <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
-      </div>
+      </div> */}
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
+          <img src={myPicture} alt="Cekiki Alimufki" style={{ width: '20px', height: '20px', marginRight: '5px' }} />
           count is {count}
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
+      <img src={myPicture} alt="My Picture" />  {/* Add this line */}
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
